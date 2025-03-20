9451dfa7ecc7f83b2c71faf8781be7cd1bb9d6ca
Michael Stowe <michaelstowe12@gmail.com>
2025-03-20 15:07:09 -0400

    Specify server port

--- apps/dashboard-server/src/main.ts
+++ apps/dashboard-server/src/main.ts
@@ -59,6 +59,7 @@ const server = createServer(
 const wss = new WebSocketServer({ server });
 injectWebSocketServer(wss);
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
